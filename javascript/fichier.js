acca93a065d870a95cb1f4d87c42302df1e6e823
melaniemdm <melanieborja.pro@gmail.com>
2021-12-12 12:11:12 +0100

    feature(js) commentaires

--- javascript/fichier.js
+++ javascript/fichier.js
@@ -1,3 +1,4 @@
+//déclaration des noeuds html en tant que variables globales et ajout des events listener associés
 const nodeBtnMag = document.querySelector(".btnMag");
 nodeBtnMag.addEventListener("click", openModalMag);
 const nodeBtnLivre = document.querySelector(".btnLivre");
@@ -14,23 +15,25 @@ const nodeInputMag = document.querySelector("#nombreExemplaireMag");
 nodeInputMag.addEventListener("change", newPrice);
 const nodeInputLivre = document.querySelector("#nombreExemplaireLivre");
 nodeInputLivre.addEventListener("change", newPrice);
+//déclaration des constantes prix
 const magPrice = 15;
 const livrePrice = 25;
-
+//fonction qu i permet d'ouvrir la modalMag et de fermer lamodal du livre
 function openModalMag() {
   nodeModalMag.style.display = "flex";
   nodeModalLivre.style.display = "none";
 }
-
+//fonction qu i permet d'ouvrir la modalLivre et de fermer lamodal du livreMag
 function openModalLivre() {
   nodeModalLivre.style.display = "flex";
   nodeModalMag.style.display = "none";
 }
-
+//fonction qui ferme les modal mag et livre
 function closeModal() {
   nodeModalMag.style.display = "none";
   nodeModalLivre.style.display = "none";
 }
+//fonction de calcul et d'affichage du prix
 function newPrice(e) {
   let nbrExemplaire = parseInt(e.target.value);
   if (nbrExemplaire > 0) {
